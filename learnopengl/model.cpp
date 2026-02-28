bb4cff1e0d1358020730254247e335af6d572dde
kachornpat <kchornpg@gmail.com>
2026-02-28 22:05:20 +0700

    update: model loader assimp

--- learnopengl/model.cpp
+++ learnopengl/model.cpp
@@ -2,11 +2,18 @@
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
 
+#include <glm/glm.hpp>
+
 #include <iostream>
 
 
 #include "model.h"
 #include "shader.h"
+
+Model::Model(std::string path)
+{
+	loadModel(path);
+}
 
 void Model::Draw(Shader& shader)
 {
@@ -25,4 +32,48 @@ void Model::loadModel(std::string path)
 		std::cout << "ERROR::ASSIMP::" << importer.GetErrorString() << std::endl;
 	}
 	directory = path.substr(0, path.find_last_of("/"));
+
+
+}
+
+void Model::processNode(aiNode* node, aiScene* scene)
+{
+	for (unsigned int i = 0; i < node->mNumMeshes; i++)
+	{
+		meshes.push_back(processMesh(scene->mMeshes[node->mMeshes[i]], scene));
+	}
+
+	for (unsigned int j = 0; j < node->mNumChildren; j++)
+	{
+		this->processNode(node->mChildren[j], scene);
+	}
+}
+
+Mesh Model::processMesh(aiMesh* mesh, aiScene* scene)
+{
+	std::vector<Vertex> vertices;
+	std::vector<unsigned int> indices;
+	std::vector<Texture> textures;
+
+	for (unsigned int i = 0; i < mesh->mNumVertices; i++)
+	{
+		Vertex vertice;
+		vertice.position.x = mesh->mVertices[i].x;
+		vertice.position.y = mesh->mVertices[i].y;
+		vertice.position.z = mesh->mVertices[i].z;
+
+		vertice.normal.x = mesh->mNormals[i].x;
+		vertice.normal.y = mesh->mNormals[i].y;
+		vertice.normal.z = mesh->mNormals[i].z;
+
+		if (mesh->mTextureCoords[0])
+		{
+			vertice.texCoord.x = mesh->mTextureCoords[0][i].x;
+			vertice.texCoord.y = mesh->mTextureCoords[0][i].y;
+		}
+
+		vertices.push_back(vertice);
+	}
+
+	return Mesh(vertices, indices, textures);
 }
--- learnopengl/model.h
+++ learnopengl/model.h
@@ -1,7 +1,7 @@
 #ifndef MODEL_H
 #define MODEL_H
 #include <vector>
-
+#include <assimp/Importer.hpp>
 #include "mesh.h"
 #include "shader.h"
 
@@ -9,10 +9,13 @@ class Model {
 private:
 	std::string directory;
 	std::vector<Mesh> meshes;
+	void processNode(aiNode* node, aiScene* scene);
+	Mesh processMesh(aiMesh* mesh, aiScene* scene);
 	void loadModel(std::string path);
 
 public:
-	void Draw(Shader &shader);
+	void Draw(Shader& shader);
+	Model(std::string path);
 };
 
 
